727e215ca666e372501f42413fc63edd0f59c3c9
Jonathan Gomes <jonathan.gomess1@gmail.com>
2024-02-15 00:02:24 -0300

    altera token

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,16 @@ output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
 }
+
+output "current_workspace_name" {
+  value = terraform.workspace
+}
+
+variable "TFC_RUN_ID" {
+  type    = string
+  default = ""
+}
+
+output "remote_execution_determine" {
+  value = "Remote run environment? %{if var.TFC_RUN_ID != ""}Yes%{else}No this is local%{endif}!"
+}
