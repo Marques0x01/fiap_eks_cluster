566c80c1403182b196cdf22b3de2a44dbe3fc212
jonathansantoss <jonathan.gomess1@gmail.com>
2024-03-12 14:27:12 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,26 +82,4 @@ module "eks" {
     ami_type = "AL2_x86_64"
 
   }
-
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-    }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-  }
 }
