20d9e8398757da6f216ab9a371818531fe519055
jonathansantoss <jonathan.gomess1@gmail.com>
2024-03-12 23:04:03 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -79,3 +79,25 @@ module "eks" {
 
   }
 }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
